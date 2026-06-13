4739d732ac974d0277d0f2d396aee385a183db0b
Miłosz Płocki <miloploszek@gmail.com>
2026-06-13 17:57:11 +0200

    added security

--- backend/auth.py
+++ backend/auth.py
@@ -1,5 +1,7 @@
 from datetime import timedelta, datetime, timezone
-from fastapi import HTTPException
+from typing import Annotated
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pwdlib import PasswordHash
 from pipeline.load import get_engine
 import jwt 
@@ -7,28 +9,29 @@ from jwt.exceptions import InvalidTokenError
 import os 
 from dotenv import load_dotenv
 
-
+blacklisted_tokens = set()
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 password_hash = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 engine = get_engine()
 dummy_hash = password_hash.hash("dummypassword")
 def verify_password(plain_password, hashed_password):
     return password_hash.verify(plain_password, hashed_password)
 def get_password_hash(password):
     return password_hash.hash(password)
-def create_access_token(data: dict):
+def create_access_token(data: dict, exprire_time: int = None):
+    minutes = exprire_time or ACCESS_TOKEN_EXPIRE_MINUTES
     to_encode = data.copy()
-    if ACCESS_TOKEN_EXPIRE_MINUTES:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=int(minutes))
     to_encode.update({'exp': expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 def verify_token(token: str):
+    if token in blacklisted_tokens:
+        raise HTTPException(status_code=401, detail="Token Unieważniony")
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         return payload
@@ -36,3 +39,16 @@ def verify_token(token: str):
         raise HTTPException(status_code=401, detail="Token Wygasł")
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=401, detail="Niepoprawny token")
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    payload = verify_token(token)
+    email = payload.get("sub")
+    role = payload.get("role")
+    if email is None:
+        raise HTTPException(status_code=401, detail="Niepoprawny token")
+    return {"email": email, "role": role}
+def require_role(allowed_roles: list):
+    def role_checker(current_user = Depends(get_current_user)):
+        if current_user["role"] not in allowed_roles:
+            raise HTTPException(status_code=403, detail="Brak uprawnień")
+        return current_user
+    return role_checker
--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,46 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from pipeline.load import get_engine, loadIntoAzure
 from pipeline.transform import transform, IngestEverything
-from backend.auth import create_access_token, get_password_hash, verify_password
+from fastapi import Depends, Request
+from fastapi.security import OAuth2PasswordRequestForm
+from backend.auth import get_current_user, get_password_hash, verify_password, create_access_token, require_role, verify_token
+from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import os
 from datetime import datetime
+from backend.auth import blacklisted_tokens, oauth2_scheme
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+
+limiter = Limiter(key_func=lambda request: request.client.host)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 engine = get_engine()
 pipeline_status = {}
+failed_attempts = {}
+MAX_ATTEMPTS = 5
+@app.middleware("http")
+async def add_security_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+    return response
 @app.get("/records")
-def get_records(limit: int = 50, offset: int = 0, status: str = "", purpose: str = ""):
+@limiter.limit("30/minute")
+def get_records(request: Request, current_user = Depends(require_role(["admin", "analityk"])), limit: int = 50, offset: int = 0, status: str = "", purpose: str = ""):
     q = []
-    params = ()
+    params = []
     if status:
         q.append(f"status = ?")
         params.append(status)
@@ -26,11 +55,16 @@ def get_records(limit: int = 50, offset: int = 0, status: str = "", purpose: str
         OFFSET {offset} ROWS
         FETCH NEXT {limit} ROWS ONLY
     """
-    df = pd.read_sql(query, engine, params=params)
-    df = df.fillna("")
+    try:
+        df = pd.read_sql(query, engine, params=params)
+        df = df.fillna("")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Błąd serwera")
     return df.to_dict(orient="records")
 @app.post("/pipeline/run")
-async def run_pipeline(file: UploadFile = File(...)):
+@limiter.limit("30/minute")
+async def run_pipeline(request: Request, file: UploadFile = File(...), current_user = Depends(require_role(["admin", "user"]))):
+
     global pipeline_status
     pipeline_status = {
         "status" : "running",
@@ -44,6 +78,8 @@ async def run_pipeline(file: UploadFile = File(...)):
         source = "csv"
     elif file.filename.endswith(".json"):
         source = "json"
+    else:
+        raise HTTPException(status_code=400, detail="Zły format pliku")
     #print(f"Ścieżka pliku: {f}")
     #print(f"Źródło: {source}")
     try:  
@@ -57,31 +93,61 @@ async def run_pipeline(file: UploadFile = File(...)):
     except Exception as e:
         pipeline_status = {
         "status" : "failed",
-        "errors" : str(e),
+        "errors" : "Błąd przetwarzania danych",
         "start" : datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         }
+        raise HTTPException(status_code=500, detail="Błąd serwera")
     os.remove(path)
-    return {"message", f"pipeline run, ingested {len(df)} records"}
+    return {"message": f"pipeline run, ingested {len(df)} records"}
 @app.get("/pipeline/status")
-def get_pipeline_status():
+@limiter.limit("30/minute")
+def get_pipeline_status(request: Request, current_user = Depends(require_role(["admin"]))):
+    if not pipeline_status:
+        return {"status": "Pipeline nie był jeszcze uruchomiony"}
     return pipeline_status
 @app.post("/register")
-def register_user(email: str, password: str):
-    df = pd.read_sql("SELECT * FROM users WHERE email = ?", engine, params=[(email,)])
+@limiter.limit("10/minute")
+def register_user(request: Request, email: str, password: str):
+    try:
+        df = pd.read_sql("SELECT * FROM users WHERE email = ?", engine, params=[(email,)])
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Błąd serwera")
     if len(df) > 0:
         raise HTTPException(status_code=400, detail="Email już istnieje")
     password_h = get_password_hash(password)
     dict = {'email': email, 'password_hash' : password_h}
     df = pd.DataFrame(dict,index=[0])
     loadIntoAzure('users', df)
-    return {"message", "registered user"}
+    return {"message": "registered user"}
 @app.post("/login")
-def login_user(email: str, password: str):
-    df = pd.read_sql("SELECT * FROM users WHERE email = ?", engine, params=[(email,)])
+@limiter.limit("10/minute")
+def login_user(request: Request, form_data: OAuth2PasswordRequestForm = Depends()):
+    email = form_data.username
+    password = form_data.password
+    try:
+        df = pd.read_sql("SELECT * FROM users WHERE email = ?", engine, params=[(email,)])
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Błąd serwera")
     if len(df) == 0:
         raise HTTPException(status_code=401, detail="Niepoprawny email lub hasło")
     correct = verify_password(password, df['password_hash'].iloc[0])
     if not correct:
+        failed_attempts[email] = failed_attempts.get(email, 0) + 1
+        if failed_attempts[email] >= MAX_ATTEMPTS:
+            raise HTTPException(status_code=403, detail="Konto zablokowane")
         raise HTTPException(status_code=401, detail="Niepoprawny email lub hasło")
-    token = create_access_token({"sub": df['email'].iloc[0], "role": df['role'].iloc[0]})
-    return {"access_token": token, "token_type" : "bearer"}
+    failed_attempts[email] = 0 
+    access_token = create_access_token({"sub": df['email'].iloc[0], "role": df['role'].iloc[0]})
+    refresh_token = create_access_token({"sub" : email}, exprire_time=60*24*7)
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type" : "bearer"}
+@app.post("/logout")
+@limiter.limit("10/minute")
+def logout(request: Request, token: str = Depends(oauth2_scheme)):
+    blacklisted_tokens.add(token)
+    return {"message": "Wylogowano"}
+@app.post("/refresh")
+@limiter.limit("10/minute")
+def refresh(request: Request, token: str = Depends(oauth2_scheme)):
+    payload = verify_token(token)
+    new_token = create_access_token({"sub": payload["sub"], "role" : payload.get("role", "user")})
+    return {"access_token" : new_token, "token_type" : "bearer"}
